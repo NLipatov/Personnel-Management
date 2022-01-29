271cd9b7a082c07f621033bfe6f9562494f375d2
NLipatov <lipatovnn@yahoo.com>
2022-01-29 19:48:10 +0300

    Initial commit

---
+++ my-app/src/components/app/app.js
@@ -0,0 +1,32 @@
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployersList from '../employers-list/employers-list';
+import EmployersAddForm from '../employers-add-form/employers-add-form';
+
+import './app.css';
+
+function App(){
+
+    const data = [
+        {name: "Fred Durst", salary: 800, increase: false, id: 1},
+        {name: "Wes Borland", salary: 3000, increase: false, id: 2},
+        {name: "John Otto", salary: 15000, increase: true, id: 3}
+    ];
+    
+    return(
+        <div className="app">
+            <AppInfo/>
+
+            <div className="search-panel">
+                <SearchPanel/>
+                <AppFilter/>
+            </div>
+
+            <EmployersList data={data}/>
+            <EmployersAddForm/>
+        </div>
+    );
+}
+
+export default App;
