f8b2c990a105d5908cac1585427ac4870f2ba5f5
Lipatov Nikita <lipatovnn@yahoo.com>
2022-01-30 15:00:22 +0300

    Deleting from data list

--- my-app/src/components/app/app.js
+++ my-app/src/components/app/app.js
@@ -3,30 +3,49 @@ import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
 import EmployersList from '../employers-list/employers-list';
 import EmployersAddForm from '../employers-add-form/employers-add-form';
-
+import {Component} from 'react';
 import './app.css';
 
-function App(){
+class App extends Component{
 
-    const data = [
-        {name: "Fred Durst", salary: 800, id: 1},
-        {name: "Wes Borland", salary: 3000, id: 2},
-        {name: "John Otto", salary: 15000, id: 3}
-    ];
+    constructor(props){
+        super(props);
+        this.state = {
+            data: [
+                {name: "Fred Durst", salary: 800, id: 1},
+                {name: "Wes Borland", salary: 3000, id: 2},
+                {name: "John Otto", salary: 15000, id: 3}
+            ]
+        }
+    }
+
+    delteItem = (id) => {
+        this.setState(({data})=> {
+            return {
+                data: data.filter(item => item.id !== id)
+            }
+        })
+    }
     
-    return(
-        <div className="app">
-            <AppInfo/>
-
-            <div className="search-panel">
-                <SearchPanel/>
-                <AppFilter/>
+    render(){
+        const {data} = this.state;
+        return(
+            <div className="app">
+                <AppInfo/>
+    
+                <div className="search-panel">
+                    <SearchPanel/>
+                    <AppFilter/>
+                </div>
+    
+                <EmployersList 
+                    data={data}
+                    onDelete={this.delteItem}/>
+                <EmployersAddForm/>
             </div>
+        );
+    }
 
-            <EmployersList data={data}/>
-            <EmployersAddForm/>
-        </div>
-    );
 }
 
 export default App;
