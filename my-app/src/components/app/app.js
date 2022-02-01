b02c48ad79ce4a33ecaa31592c07caf70924ed9f
Lipatov Nikita <lipatovnn@yahoo.com>
2022-02-01 14:25:57 +0300

    Employees search

--- my-app/src/components/app/app.js
+++ my-app/src/components/app/app.js
@@ -16,7 +16,7 @@ class App extends Component{
                 {name: "Wes Borland", salary: 3000, increase: true, rise: false, id: 2},
                 {name: "John Otto", salary: 15000, increase: false, rise: false, id: 3}
             ],
-            test: "testString"
+            term: ""
         }
     }
 
@@ -96,9 +96,24 @@ class App extends Component{
             })
         }))
     }
+
+    searchEmp = (items, term) => {
+        if (term.length === 0){
+            return items;
+        }
+
+        return items.filter(x=> {
+            return x.name.indexOf(term) > -1;
+        });
+    }
+
+    onUpdateSearch = (term) => {
+        this.setState({term});
+    }
     
     render(){
-        const {data} = this.state;
+        const {data, term} = this.state;
+        const visibleData = this.searchEmp(data, term);
         this.getLastId()
         return(
             <div className="app">
@@ -106,12 +121,13 @@ class App extends Component{
                     data={data}/>
     
                 <div className="search-panel">
-                    <SearchPanel/>
+                    <SearchPanel
+                        onUpdateSearch={this.onUpdateSearch}/>
                     <AppFilter/>
                 </div>
     
                 <EmployersList 
-                    data={data}
+                    data={visibleData}
                     onDelete={this.deleteItem}
                     onToggleProp={this.onToggleProp}/>
                 <EmployersAddForm
